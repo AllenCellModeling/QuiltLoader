add5a9a04014577892f41df693575fd062a8ca36
JacksonMaxfield <jacksonb@alleninstitute.org>
2018-04-18 14:56:03 -0700

    added __init__.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 from setuptools import setup
 
-setup(name='QuiltLoader',
+setup(name='quiltloader',
       description=' adds bio-science loaders to quilt nodes as well as niceties for interacting and navigating quilt nodes',
       author='Jackson Maxfield Brown',
       author_email='jacksonb@alleninstitute.org',
       url='https://github.com/AllenCellModeling/QuiltLoader',
-      packages=['QuiltLoader'],
+      packages=['quiltloader'],
       install_requires=['quilt','matplotlib', 'numpy', 'tifffile'])
